de87604d7550909395652cc9b18f066acfe955cc
cdub619 <chriswilliamson@live.com>
2022-12-18 18:09:00 -0800

    Updates

--- LoseControlWarning.lua
+++ LoseControlWarning.lua
@@ -1,9 +1,6 @@
 --GIT HUB
 
 local scf = { }
-local buffs1 = {}
-local buffs2 = {}
-
 local hieght = 30
 local width = 30
 
@@ -33,55 +30,31 @@ local spellIds = {
 
 
 local LoseControlWarning = CreateFrame('Frame')
-LoseControlWarning:SetScript('OnEvent', function(self, event, ...)
-	self[event](self, ...)
+LoseControlWarning:SetScript('OnEvent', function(self, event, arg1, arg2)
+	if event == 'PLAYER_ENTERING_WORLD' then LoseControlWarning:PLAYER_ENTERING_WORLD()
+	elseif event == 'GROUP_ROSTER_UPDATE' then LoseControlWarning:GROUP_ROSTER_UPDATE()
+	elseif event == 'GROUP_JOINED' then LoseControlWarning:GROUP_ROSTER_UPDATE()
+	elseif event == "UNIT_AURA" then self:UNIT_AURA(arg1)
+	end
 end)
 
 LoseControlWarning:RegisterEvent('PLAYER_ENTERING_WORLD')
 LoseControlWarning:RegisterEvent("GROUP_ROSTER_UPDATE")
 LoseControlWarning:RegisterEvent("GROUP_JOINED")
 
-LoseControlparty1:HookScript("OnShow", function(self)
-	LoseControlWarning:UpdateFrames("party1")
-end)
-LoseControlparty2:HookScript("OnShow", function(self)
-	LoseControlWarning:UpdateFrames("party2")
-end)
-LoseControlparty4:HookScript("OnShow", function(self)
-	LoseControlWarning:UpdateFrames("party2")
-end)
-LoseControlparty3:HookScript("OnShow", function(self)
-	LoseControlWarning:UpdateFrames("party2")
-end)
-
-LoseControlparty1:HookScript("OnHide", function(self)
-	LoseControlWarning:UpdateFrames("party1")
-end)
-LoseControlparty2:HookScript("OnHide", function(self)
-	LoseControlWarning:UpdateFrames("party2")
-end)
-LoseControlparty3:HookScript("OnHide", function(self)
-	LoseControlWarning:UpdateFrames("party2")
-end)
-LoseControlparty4:HookScript("OnHide", function(self)
-	LoseControlWarning:UpdateFrames("party2")
-end)
-
 function LoseControlWarning:PLAYER_ENTERING_WORLD()
 	local inInstance, instanceType = IsInInstance()
 	local GrSize = GetNumGroupMembers()
 	if GrSize <= 5 and (instanceType ~= "pvp" or instanceType ~= "raid") then
-		LoseControlWarning:RegisterUnitEvent('UNIT_AURA', "player")
-		LoseControlWarning:UpdateFrames("player")
-		for i = 1, GetNumGroupMembers() do
-			local unitId = "party"..i
-			if UnitExists(unitId) then
-				LoseControlWarning:RegisterUnitEvent('UNIT_AURA', unitId)
-				LoseControlWarning:UpdateFrames(unitId)
-			end
-		end
+		LoseControlWarning:RegisterEvent("UNIT_AURA")
+		LoseControlWarning:UNIT_AURA("player")
+		LoseControlWarning:UNIT_AURA("party1")
+		LoseControlWarning:UNIT_AURA("party2")
+		LoseControlWarning:UNIT_AURA("party3")
+		LoseControlWarning:UNIT_AURA("party4")
 	else
 		LoseControlWarning:UnregisterEvent("UNIT_AURA")
+		LoseControlWarning:Reset()
 	end
 end
 
@@ -89,169 +62,159 @@ function LoseControlWarning:GROUP_ROSTER_UPDATE()
 	local inInstance, instanceType = IsInInstance()
 	local GrSize = GetNumGroupMembers()
 	if GrSize <= 5 and (instanceType ~= "pvp" or instanceType ~= "raid") then
-		LoseControlWarning:RegisterUnitEvent('UNIT_AURA', "player")
-		LoseControlWarning:UpdateFrames("player")
-		for i = 1, GetNumGroupMembers() do
-			local unitId = "party"..i
-			if UnitExists(unitId) then
-				LoseControlWarning:RegisterUnitEvent('UNIT_AURA', unitId)
-				LoseControlWarning:UpdateFrames(unitId)
-			end
-		end
+		LoseControlWarning:RegisterEvent("UNIT_AURA")
+		LoseControlWarning:UNIT_AURA("player")
+		LoseControlWarning:UNIT_AURA("party1")
+		LoseControlWarning:UNIT_AURA("party2")
+		LoseControlWarning:UNIT_AURA("party3")
+		LoseControlWarning:UNIT_AURA("party4")
 	else
 		LoseControlWarning:UnregisterEvent("UNIT_AURA")
+		LoseControlWarning:Reset()
 	end
 end
 
-function LoseControlWarning:GROUP_JOINED()
-	self:GROUP_ROSTER_UPDATE()
+function LoseControlWarning:Reset()
+	for i = 1, #scf do
+		local unitId = "party"..i
+		if scf[unitId] then
+			for j = 1, #scf[unitId] do
+				if scf[unitId][j] then
+					scf[unitId][j]:Hide()
+					scf[unitId][j] = nil
+				end
+			end
+			scf[unitId] = nil
+		end
+	end
+	local unitId = "player"
+	if scf[unitId] then
+		for j = 1, #scf[unitId] do
+			if scf[unitId][j] then
+				scf[unitId][j]:Hide()
+				scf[unitId][j] = nil
+			end
+		end
+		scf[unitId] = nil
+	end
 end
 
-local function compare(t1,t2,ignore_mt)
-   local ty1 = type(t1)
-   local ty2 = type(t2)
-   if ty1 ~= ty2 then return false end
-   -- non-table types can be directly compared
-   if ty1 ~= 'table' and ty2 ~= 'table' then return t1 == t2 end
-   -- as well as tables which have the metamethod __eq
-   local mt = getmetatable(t1)
-   if not ignore_mt and mt and mt.__eq then return t1 == t2 end
-   for k1,v1 in pairs(t1) do
-      local v2 = t2[k1]
-      if v2 == nil or not compare(v1,v2) then return false end
-   end
-   for k2,v2 in pairs(t2) do
-      local v1 = t1[k2]
-      if v1 == nil or not compare(v1,v2) then return false end
-   end
-   return true
+function LoseControlWarning:CreateFrame(unitId, j)
+	local relativeFrame, relativePoint, Point, id, LoseControlparty, PartyAnchor 
+	scf[unitId][j] = CreateFrame("Frame", "LCWarning"..j..unitId, UIParent)
+	scf[unitId][j]:SetHeight(hieght)
+	scf[unitId][j]:SetWidth(width)
+	scf[unitId][j].texture = scf[unitId][j]:CreateTexture(nil, 'BACKGROUND')
+	scf[unitId][j].texture:SetAllPoints(scf[unitId][j])
+	scf[unitId][j].texture:SetTexCoord(0.01, .99, 0.01, .99) -- smallborder
+	scf[unitId][j].cooldown = CreateFrame("Cooldown", nil, scf[unitId][j], 'CooldownFrameTemplate')
+	scf[unitId][j].cooldown:SetEdgeTexture("Interface\\Cooldown\\edge")    --("Interface\\Cooldown\\edge-LoC") Blizz LC CD
+	scf[unitId][j].cooldown:SetDrawSwipe(true)
+	scf[unitId][j].cooldown:SetDrawEdge(false)
+	scf[unitId][j].cooldown:SetSwipeColor(0.17, 0, 0)
+	scf[unitId][j].cooldown:SetReverse(true) --will reverse the swipe if actionbars or debuff, by default bliz sets the swipe to actionbars if this = true it will be set to debuffs
+	scf[unitId][j].cooldown:SetDrawBling(false)
+	scf[unitId][j].cooldown:SetAllPoints(scf[unitId][j])
+	scf[unitId][j].count = scf[unitId][j]:CreateFontString(nil, "OVERLAY","NumberFontNormal");
+	scf[unitId][j].count:SetPoint("BOTTOMRIGHT",-2,1);
+	scf[unitId][j].count:SetJustifyH("RIGHT");
+	if j == 1 then
+		if unitId ~= "player" then
+			id = strmatch(unitId, '%d')
+			LoseControlparty = _G["LoseControlparty"..id]
+			if unitId == "party1" then PartyAnchor = PartyAnchor1 end
+			if unitId == "party2" then PartyAnchor = PartyAnchor2 end
+			if unitId == "party3" then PartyAnchor = PartyAnchor3 end
+			if unitId == "party4" then PartyAnchor = PartyAnchor4 end
+			LoseControlparty:HookScript("OnShow", function(self)
+				relativeFrame = LoseControlparty;relativePoint = "BOTTOMLEFT";Point = "BOTTOMRIGHT"
+				scf[unitId][j]:ClearAllPoints()
+				scf[unitId][j]:SetParent(relativeFrame)
+				scf[unitId][j]:SetPoint(Point, relativeFrame, relativePoint, 0, 0)
+			end)
+			LoseControlparty:HookScript("OnHide", function(self)
+				relativeFrame = PartyAnchor;relativePoint = "BOTTOMRIGHT";Point = "BOTTOMRIGHT"
+				scf[unitId][j]:ClearAllPoints()
+				scf[unitId][j]:SetParent(relativeFrame)
+				scf[unitId][j]:SetPoint(Point, relativeFrame, relativePoint, 0, 0)
+			end)
+			scf[unitId][j]:SetScript("OnShow", function()
+				if LoseControlparty:IsShown() then
+					relativeFrame = LoseControlparty;relativePoint = "BOTTOMLEFT";Point = "BOTTOMRIGHT"
+				else
+					relativeFrame = PartyAnchor;relativePoint = "BOTTOMRIGHT";Point = "BOTTOMRIGHT"
+				end
+				scf[unitId][j]:ClearAllPoints()
+				scf[unitId][j]:SetParent(relativeFrame)
+				scf[unitId][j]:SetPoint(Point, relativeFrame, relativePoint, 0, 0)
+			end)
+		end
+		if unitId == "player" then
+			relativeFrame = PartyAnchor5
+			relativePoint = "BOTTOMRIGHT"
+			Point = "BOTTOMRIGHT"
+		 elseif strmatch(unitId, "party") then
+			id = strmatch(unitId, '%d')
+			LoseControlparty = _G["LoseControlparty"..id]
+			if unitId == "party1" then PartyAnchor = PartyAnchor1 end
+			if unitId == "party2" then PartyAnchor = PartyAnchor2 end
+			if unitId == "party3" then PartyAnchor = PartyAnchor3 end
+			if unitId == "party4" then PartyAnchor = PartyAnchor4 end
+			if LoseControlparty:IsShown() then
+				relativeFrame = LoseControlparty
+				relativePoint = "BOTTOMLEFT"
+				Point = "BOTTOMRIGHT"
+			else
+				relativeFrame = PartyAnchor
+				relativePoint = "BOTTOMRIGHT"
+				Point = "BOTTOMRIGHT"
+			end
+		end
+		scf[unitId][j]:ClearAllPoints()
+		scf[unitId][j]:SetParent(relativeFrame)
+		scf[unitId][j]:SetPoint(Point, relativeFrame, relativePoint, 0, 0)
+	else
+		scf[unitId][j]:SetParent(scf[unitId][j-1])
+		scf[unitId][j]:SetPoint("BOTTOMRIGHT", scf[unitId][j-1], "BOTTOMLEFT",0,0)
+	end
 end
 
 function LoseControlWarning:UNIT_AURA(unitId)
-	local k = 1
-
-	if not buffs1[unitId] then
-		buffs1[unitId] = {}
-	end
-	if not buffs2[unitId] then
-		buffs2[unitId] = {}
-	end
-
-	if #buffs1[unitId] > 0 then
-		for i = 1, #buffs1[unitId] do
-				buffs1[unitId][i] = nil
-		end
-	end
-
-	for i = 1, 40 do
-		local name, icon, count, _, duration, expirationTime, _, _, _, spellId = UnitAura(unitId, i,  "HARMFUL")
-		if not spellId then break end
-		if spellIds[spellId] then
-		table.insert(buffs1[unitId], k , expirationTime )
-			k = k + 1
-		end
-	end
-
-	if #buffs1[unitId] ~= #buffs2[unitId] then
-		LoseControlWarning:UpdateFrames(unitId)
-		if #buffs2[unitId] > 0 then
-			for i = 1, #buffs2[unitId] do
-				buffs2[unitId][i] = nil
-			end
-		end
-		if #buffs1[unitId] > 0 then
-			for i = 1, #buffs1[unitId] do
-				buffs2[unitId][i] = buffs1[unitId][i]
-			end
-  	end
-	else
-		if compare(buffs1[unitId], buffs2[unitId]) then
-		else
-			LoseControlWarning:UpdateFrames(unitId)
-			if #buffs2[unitId] > 0 then
-				for i = 1, #buffs2[unitId] do
-					buffs2[unitId][i] = nil
+	if (unitId == "player" or strmatch(unitId, "party")) and not strmatch(unitId, "partypet") then
+		local j = 1;
+		if not scf[unitId] then scf[unitId] = {} end
+		for i = 1, 40 do
+			local name, icon, count, _, duration, expirationTime, _, _, _, spellId = UnitAura(unitId, i,  "HARMFUL")
+			if spellIds[spellId] then
+				if not scf[unitId][j] then 
+					LoseControlWarning:CreateFrame(unitId, j)
 				end
-			end
-			if #buffs1[unitId] > 0 then
-				for i = 1, #buffs1[unitId] do
-					buffs2[unitId][i] = buffs1[unitId][i]
+				scf[unitId][j].texture:SetTexture(icon)
+				local enabled = expirationTime and expirationTime ~= 0;
+				if enabled then
+					local startTime = expirationTime - duration;
+					scf[unitId][j].cooldown:SetCooldown( startTime, duration)
 				end
-			end
-		end
-	end
-end
-
-function LoseControlWarning:UpdateFrames(unitId)
-	local j = 1; local relativeFrame, relativePoint, Point
-	for i = 1, 40 do
-		local name, icon, count, _, duration, expirationTime, _, _, _, spellId = UnitAura(unitId, i,  "HARMFUL")
-		if scf[i..unitId] then
-			scf[i..unitId]:Hide()
-		end
-		if spellIds[spellId] then
-			if unitId == "player" or strmatch(unitId, "party") then
-				if not scf[j..unitId] then
-					scf[j..unitId] = CreateFrame("Frame", "LCWarning"..j..unitId, UIParent)
-					scf[j..unitId]:SetHeight(hieght)
-					scf[j..unitId]:SetWidth(width)
-					scf[j..unitId].texture = scf[j..unitId]:CreateTexture(nil, 'BACKGROUND')
-					scf[j..unitId].texture:SetAllPoints(scf[j..unitId])
-					scf[j..unitId].texture:SetTexCoord(0.01, .99, 0.01, .99) -- smallborder
-					scf[j..unitId].cooldown = CreateFrame("Cooldown", nil, scf[j..unitId], 'CooldownFrameTemplate')
-					scf[j..unitId].cooldown:SetEdgeTexture("Interface\\Cooldown\\edge")    --("Interface\\Cooldown\\edge-LoC") Blizz LC CD
-					scf[j..unitId].cooldown:SetDrawSwipe(true)
-					scf[j..unitId].cooldown:SetDrawEdge(false)
-					scf[j..unitId].cooldown:SetSwipeColor(0.17, 0, 0)
-					scf[j..unitId].cooldown:SetReverse(true) --will reverse the swipe if actionbars or debuff, by default bliz sets the swipe to actionbars if this = true it will be set to debuffs
-					scf[j..unitId].cooldown:SetDrawBling(false)
-					scf[j..unitId].cooldown:SetAllPoints(scf[j..unitId])
-					scf[j..unitId].count = scf[j..unitId]:CreateFontString(nil, "OVERLAY","NumberFontNormal");
-					scf[j..unitId].count:SetPoint("BOTTOMRIGHT",-2,1);
-					scf[j..unitId].count:SetJustifyH("RIGHT");
-				end
-				scf[j..unitId].texture:SetTexture(icon)
-				scf[j..unitId].cooldown:SetCooldown( expirationTime - duration, duration)
 				if count then
 					if ( count > 1 ) then
 						local countText = count
 						if ( count >= 100 ) then
-						 countText = BUFF_STACKS_OVERFLOW
+							countText = BUFF_STACKS_OVERFLOW
 						end
-						scf[j..unitId].count:Show();
-						scf[j..unitId].count:SetText(countText)
+						scf[unitId][j].count:Show();
+						scf[unitId][j].count:SetText(countText)
 					else
-				  	scf[j..unitId].count:Hide();
+					scf[unitId][j].count:Hide();
 					end
 				end
-				if unitId == "player" then
-					relativeFrame = PartyAnchor5
-					relativePoint = "BOTTOMRIGHT"
-					Point = "BOTTOMRIGHT"
-		 		elseif strmatch(unitId, "party") then
-					local id = strmatch(unitId, '%d')
-					LoseControlparty, PartyAnchor = _G["LoseControlparty"..id], _G["PartyAnchor"..id]
-					if LoseControlparty:IsShown() then
-						relativeFrame = LoseControlparty
-						relativePoint = "BOTTOMLEFT"
-						Point = "BOTTOMRIGHT"
-					else
-						relativeFrame = PartyAnchor
-						relativePoint = "BOTTOMRIGHT"
-						Point = "BOTTOMRIGHT"
-					end
-				end
-				if j == 1 then
-					scf[j..unitId]:ClearAllPoints()
-					scf[j..unitId]:SetParent(relativeFrame)
-					scf[j..unitId]:SetPoint(Point, relativeFrame, relativePoint, 0, 0)
-				else
-					scf[j..unitId]:SetParent(relativeFrame)
-					scf[j..unitId]:SetPoint("BOTTOMRIGHT", scf[(j-1)..unitId], "BOTTOMLEFT",0,0)
-				end
-				scf[j..unitId]:Show();
+				scf[unitId][j]:Show();
 				j = j + 1
 			end
 		end
-  end
+		for i = j, #scf[unitId] do
+			if scf[unitId][i] then
+				scf[unitId][i]:Hide()
+			end
+		end
+	end
 end
